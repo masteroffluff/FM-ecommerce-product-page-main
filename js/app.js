f5eb7fcb65569bce93644996ce78a970bddf01b8
Chris Chapman <chris.chapman@linuxmail.org>
2025-01-24 21:07:39 +0000

    added first batch of vue js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,57 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2/dist/vue.esm.browser.js';
-import data from data.js
+
+import {shoes} from './data.js';
+
+
 
 const app = new Vue({
-    el: '#app'
-  });
+  el: '#app',
+  data: {
+    avatar: './images/image-avatar.png',
+    product: shoes[0],
+    amount:0,
+    cart:new Map(),
+    activeImageNo:0,
+    thumbnailClass:"product-thumbnail"
+  },
+  computed:{
+    mainImageUrl: function() {return "/images/" + this.product.images[this.activeImageNo].main}
+  },
+  methods:{
+    orderPlus:function(){
+      this.amount++
+    },
+    orderMinus:function(){
+      this.amount--
+      if(this.amount<0){this.amount=0}
+    },
+    addToCart:function(){
+      const {sku, productName} = this.product
+      if(this.car.has(this.product)){
+        const currentAmount = this.cart.get(this.product)
+        this.cart.set(this.product,this.amount + currentAmount)
+      } else {
+        this.cart.set(this.product,this.amount)
+      }
+    },
+    imageNext:function(){
+      this.activeImageNo++
+      if(this.activeImageNo>=this.product.images.length){this.activeImageNo=this.product.images.length-1}
+
+    },
+    imagePrev:function(){
+      this.activeImageNo--
+      if(this.activeImageNo<0){this.activeImageNo=0}
+    },
+    activateImage:function(e){
+      console.log(e.currentTarget)
+      this.activeImageNo = parseInt(e.currentTarget.value, 10)||0;
+      console.log(`Active image set to: ${this.activeImageNo}`);
+    }
+
+  }
+});
+
+
+
+
---
+++ js/data.js
@@ -0,0 +1,31 @@
+export const shoes = [
+  {
+    sku: 0,
+    manufacturer: "Sneaker Company",
+    productName: "Fall Limited Edition Sneakers",
+    productDesc:
+      "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
+    RRP: 250,
+    discount: "50%",
+    currentRP: 125,
+    collections: ["women"],
+    images: [
+      {
+        main: "image-product-1.jpg",
+        thumb: "image-product-1-thumbnail",
+      },
+      {
+        main: "image-product-2.jpg",
+        thumb: "image-product-2-thumbnail",
+      },
+      {
+        main: "image-product-3.jpg",
+        thumb: "image-product-3-thumbnail",
+      },
+      {
+        main: "image-product-4.jpg",
+        thumb: "image-product-4-thumbnail",
+      },
+    ],
+  },
+];
